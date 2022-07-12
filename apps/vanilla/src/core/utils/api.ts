d06ead169267796dfc4d9f3a70d9ec78d929f3c4
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-07-12 17:02:27 +0700

    Adding configDefault for pagination Task JC19-327

--- apps/vanilla/src/core/utils/api.ts
+++ apps/vanilla/src/core/utils/api.ts
@@ -8,7 +8,9 @@ import { AnimeDto } from '@js-camp/core/dtos/anime.dto';
 
 import { PaginationDto } from '@js-camp/core/dtos/pagination.dto';
 
-import { CURRENT_PAGE_DEFAULT, Ordering, PAGE_SIZE_DEFAULT } from '../constants/anime';
+import { FIRST_PAGE, PAGE_SIZE_DEFAULT } from '../constants/anime';
+
+import { Ordering } from '../enums/table';
 
 import { apiAnime } from './axiosInstance';
 
@@ -20,21 +22,27 @@ import { apiAnime } from './axiosInstance';
 export interface PaginationConfig {
 
   /** The number of results returned per page. */
-  currentPage: number;
+  readonly currentPage: number;
 
   /** Current page. */
-  ordering: Ordering;
+  readonly ordering: Ordering;
 
-  /** Sorting mode. */
+  /** Limit page. */
   readonly limit?: number;
 }
+
+const configDefault = {
+  currentPage: FIRST_PAGE,
+  ordering: Ordering.None,
+  limit: PAGE_SIZE_DEFAULT,
+};
 
 /**
  * Reception function with a configured URL.
  * @param paginationConfig Parameters for getting anime from the database.
  */
 export async function getAnimeData(paginationConfig: PaginationConfig): Promise<Pagination<Anime>> {
-  const { currentPage = CURRENT_PAGE_DEFAULT, ordering = Ordering.None, limit = PAGE_SIZE_DEFAULT } = paginationConfig;
+  const { currentPage = configDefault.currentPage, ordering = configDefault.ordering, limit = configDefault.limit } = paginationConfig;
   const offset = (currentPage - 1) * limit;
   const urlAnime = new URLSearchParams(`limit=${limit}&offset=${offset}&ordering=${ordering.concat(',')}id`);
   const response = await apiAnime.get<PaginationDto<AnimeDto>>(
