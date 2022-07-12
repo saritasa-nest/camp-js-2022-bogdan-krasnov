ee0335267798ab9d1b74f76abc979977b725d430
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-07-12 10:08:57 +0700

    Fix api.ts Task JC19-327

--- apps/vanilla/src/core/utils/api.ts
+++ apps/vanilla/src/core/utils/api.ts
@@ -2,24 +2,37 @@ import { PaginationMapper } from '@js-camp/core/mappers/pagination.mapper';
 import { Anime } from '@js-camp/core/models/anime';
 import { Pagination } from '@js-camp/core/models/pagination';
 
-import { CURRENT_PAGE_DEFAULT, ORDERING_DEFAULT, PAGE_SIZE_DEFAULT } from '../constants/anime';
+import { CURRENT_PAGE_DEFAULT, Ordering, PAGE_SIZE_DEFAULT } from '../constants/anime';
+
 import { apiAnime } from './axiosInstance';
 
-/**
- * Reception function with a configured URL.
+/** Parameters for getting anime from the database.
  * @param currentPage Current page.
  * @param ordering Ordering page.
  * @param limit Size page.
- * @param offset Offset page.
  */
-export async function getAnimeData(
-  currentPage = CURRENT_PAGE_DEFAULT,
-  ordering = ORDERING_DEFAULT,
-  limit = PAGE_SIZE_DEFAULT,
-  offset = ((currentPage - 1) * limit),
-): Promise<Pagination<Anime>> {
+interface PaginationConfig {
+
+  /** The number of results returned per page. */
+  readonly currentPage: number;
+
+  /** Current page. */
+  readonly ordering: Ordering;
+
+  /** Sorting mode. */
+  readonly limit: number;
+}
+
+/**
+ * Reception function with a configured URL.
+ * @param paginationConfig Parameters for getting anime from the database.
+ */
+export async function getAnimeData(paginationConfig: PaginationConfig): Promise<Pagination<Anime>> {
+  const { currentPage = CURRENT_PAGE_DEFAULT, ordering = Ordering.None, limit = PAGE_SIZE_DEFAULT } = paginationConfig;
+  const offset = (currentPage - 1) * paginationConfig.limit;
+  const urlAnime = new URLSearchParams(`limit=${limit}&offset=${offset}&ordering=${ordering.concat(',')}id`);
   const response = await apiAnime.get(
-    `/anime/anime/?limit=${limit}&offset=${offset}&ordering=${ordering.concat(',')}id`,
+    `/anime/anime/?${urlAnime}`,
   );
   const { data } = response;
   return PaginationMapper.fromDto(data);
