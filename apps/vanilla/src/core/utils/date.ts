b827945182e212bf8708d655cd692ab9f773d909
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-07-12 10:35:47 +0700

    Creating date.ts Task JC19-327

---
+++ apps/vanilla/src/core/utils/date.ts
@@ -0,0 +1,13 @@
+/**
+ * The function of converting data to date format.
+ * @param dateReceived Data coming from the server .
+ */
+export function formatDate(dateReceived: Date): string {
+  const date = new Date(dateReceived);
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  };
+  return date.toLocaleString('ru', options);
+}
