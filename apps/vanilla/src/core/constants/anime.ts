a01ccf931f515cd6454f6bcd8bacbee262f534c1
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-07-12 17:01:09 +0700

    Adding enum Ordering Task JC19-327

--- apps/vanilla/src/core/constants/anime.ts
+++ apps/vanilla/src/core/constants/anime.ts
@@ -1,13 +1,11 @@
+import { Ordering } from '../enums/table';
+
 export const PAGE_SIZE_DEFAULT = 5;
 
-/** Possible sorting options. */
-export enum Ordering {
-  None = '',
-  TitleEng = '-title_eng',
-  Aired = 'aired',
-  Status = 'status',
-}
+export const ORDERING_DEFAULT = Ordering.None;
 
 export const CURRENT_PAGE_DEFAULT = 1;
+
+export const FIRST_PAGE = 1;
 
 export const QUANTITY_ANIME = 24200;
---
+++ apps/vanilla/src/core/enums/table.ts
@@ -0,0 +1,7 @@
+/** Possible sorting options. */
+export enum Ordering {
+  None = '',
+  TitleEng = '-title_eng',
+  Aired = 'aired',
+  Status = 'status',
+}
