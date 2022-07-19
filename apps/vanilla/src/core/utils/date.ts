f29a007a874ec0b4016f51a9c46ea3131618ae6b
bogdan <bogdan.krasnov@saritasa.com>
2022-07-19 11:16:07 +0700

    Fix date.ts Task JC19-327

--- apps/vanilla/src/core/utils/date.ts
+++ apps/vanilla/src/core/utils/date.ts
@@ -1,15 +1,15 @@
 /**
- * Converting data to string format.
+ * Converting data to string format DD-MM-YYYY.
  * @param date Date.
  */
-export function formatDate(date: Date | null): string {
+export function formatDate(date: Date | null): null | string {
   const options: Intl.DateTimeFormatOptions = {
     year: 'numeric',
     month: '2-digit',
     day: '2-digit',
   };
   if (date === null) {
-    return 'No date';
+    return null;
   }
   return date.toLocaleString('ru', options);
 }
