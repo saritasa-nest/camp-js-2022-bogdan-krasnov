8e12bbc63bafc212b5b4d76d0d0dc9c4c0b6bdd8
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-07-21 16:35:46 +0700

    Create anime-table component Task JC19-393

---
+++ apps/angular/src/app/features/anime-table/anime-table.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'camp-anime-table',
+  templateUrl: './anime-table.component.html',
+  styleUrls: ['./anime-table.component.css'],
+})
+
+/**
+ * Anime table component.
+ */
+export class AnimeTableComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+}
+
