8a27996f28a2375e858e661fb9a0ae43a57e44d7
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-07-22 15:38:41 +0700

    Create a table view Task JC19-393

--- apps/angular/src/app/features/anime-table/anime-table.component.ts
+++ apps/angular/src/app/features/anime-table/anime-table.component.ts
@@ -1,20 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Anime } from '@js-camp/core/models/anime';
+import { BehaviorSubject, switchMap, Observable, tap } from 'rxjs';
 
-
-@Component({
-  selector: 'camp-anime-table',
-  templateUrl: './anime-table.component.html',
-  styleUrls: ['./anime-table.component.css'],
-})
+import { AnimeService } from './../../../core/services/anime.service';
 
 /**
  * Anime table component.
  */
+@Component({
+  selector: 'camp-anime-table',
+  templateUrl: './anime-table.component.html',
+  styleUrls: ['./anime-table.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
 export class AnimeTableComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {
+  /** Anime. */
+  public readonly animeList$: Observable<readonly Anime[]>;
 
+  /** Whether books are loading or not. */
+  public readonly isAnimeLoading$ = new BehaviorSubject<boolean>(false);
+
+  /** Displayed columns. */
+  public displayedColumns: string[] = ['id', 'titleEnglish', 'type', 'status'];
+
+  private readonly animeUpdated$ = new BehaviorSubject<void>(void 0);
+
+  public constructor(animeService: AnimeService) {
+    this.animeList$ = this.animeUpdated$.pipe(
+      tap(() => this.isAnimeLoading$.next(true)),
+      switchMap(() => animeService.getAnimeList()),
+      tap(() => this.isAnimeLoading$.next(false)),
+    );
+  }
+
+  /** On init. */
+  public ngOnInit(): void {
+    this.animeList$.pipe(
+      tap(anime => console.log(anime)),
+    ).subscribe();
   }
 }
-
