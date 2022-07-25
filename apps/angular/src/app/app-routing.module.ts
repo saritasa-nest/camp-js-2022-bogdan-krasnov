5df8884faac99e623835a180dc5d69716d58ca12
bogdan <bogdan.krasnov@saritasa.com>
2022-07-25 22:38:32 +0700

    Add routing Task JC19-393

--- apps/angular/src/app/app-routing.module.ts
+++ apps/angular/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { NotFoundComponent } from './../shared/components/not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnimeTableComponent } from './features/anime-table/anime-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AnimeTableComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  }
+];
 
 /** App routing module. */
 @NgModule({
