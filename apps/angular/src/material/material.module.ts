d8a9aa52c5e9c0848b09c2282805db2a3c44282f
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-08-04 15:02:12 +0700

    Update material.module Task JC19-425

--- apps/angular/src/material/material.module.ts
+++ apps/angular/src/material/material.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 const material = [
   MatTableModule,
+  MatButtonModule,
+  MatInputModule,
+  MatFormFieldModule,
+  FormsModule,
+  ReactiveFormsModule,
 ];
 
 /** Material Angular Module. */
