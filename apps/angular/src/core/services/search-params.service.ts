d1b7d2c1c1912deff9c212e1fe248e073fda697f
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-08-05 09:19:23 +0700

    Add search-params Task JC19-405

---
+++ apps/angular/src/core/services/search-params.service.ts
@@ -0,0 +1,47 @@
+import { HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+import { PaginationParams } from '../models/pagination-params';
+
+/** Construct and redirecting by query search params. */
+@Injectable({
+  providedIn: 'root',
+})
+export class SearchParamsService {
+
+  public constructor(
+    private readonly router: Router,
+    private readonly activatedRoute: ActivatedRoute,
+  ) { }
+
+  /**
+   * Creates search params for anime list GET query.
+   * @param pagination Pagination params.
+   */
+  public changeSearchParams(pagination: PaginationParams): HttpParams {
+
+    const newSearchParams = { ...pagination };
+
+    const filteredNewSearchParams: Params = [];
+    for (const [name, value] of Object.entries(newSearchParams)) {
+      if (value !== '' && value !== 0) {
+        filteredNewSearchParams[name] = value;
+      }
+    }
+
+    this.router.navigate([], {
+      queryParams: filteredNewSearchParams,
+    });
+
+    return new HttpParams({
+      fromObject: filteredNewSearchParams,
+    });
+  }
+
+  /** Gets search params from browser address string. */
+  public getAnimeListSearchParams(): Params {
+    const { queryParams } = this.activatedRoute.snapshot;
+    return queryParams;
+  }
+}
