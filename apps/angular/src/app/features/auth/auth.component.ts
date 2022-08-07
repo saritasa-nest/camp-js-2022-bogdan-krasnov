8b7010f8a27fbf815ed9e3460e85f8f3a370ae8a
bogdan <bogdan.krasnov@saritasa.com>
2022-08-07 20:55:25 +0700

    Add auth.component.html Task JC19-425

--- apps/angular/src/app/features/auth/auth.component.ts
+++ apps/angular/src/app/features/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'camp-registration',
+  templateUrl: './auth.component.html',
 })
 export class AuthComponent{
   constructor() {}
