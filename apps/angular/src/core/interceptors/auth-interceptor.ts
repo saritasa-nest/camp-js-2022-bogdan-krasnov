9970a001ffe91b9f8f1907187c06d717265fcab4
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-08-08 16:09:14 +0700

    Update auth-interceptor Task JC19-425

--- apps/angular/src/core/interceptors/auth-interceptor.ts
+++ apps/angular/src/core/interceptors/auth-interceptor.ts
@@ -9,57 +9,33 @@ import { Observable } from 'rxjs';
 
 import { AppConfigService } from '../services/app-config.service';
 
+import { AuthService } from './../services/auth.service';
+
 /** Interceptor to add access token to requests using Authorization HTTP header. */
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   public constructor(
     private readonly appConfigService: AppConfigService,
-
-    // private readonly userSecretStorage: UserSecretStorageService,
-    // private readonly authService: AuthService,
+    private readonly authService: AuthService,
   ) {}
 
   /**
    * Appends bearer token.
    * @inheritdoc
    */
-  intercept(
+  public intercept(
     req: HttpRequest<unknown>,
     next: HttpHandler,
   ): Observable<HttpEvent<unknown>> {
-      const token = localStorage.getItem('token');
-
-      if (token) {
-        const clonedReq = req.clone({
-          headers: req.headers.set('Authorization',
-            `Bearer ${token}`),
-        });
-        return next.handle(clonedReq);
-      }
-      return next.handle(req);
-
-      // const userSecret$ = this.userSecretStorage.currentSecret$.pipe(first());
-
-      // return userSecret$.pipe(
-      //   map(userSecret =>
-      //     userSecret ?
-      //       req.clone({
-      //           headers: this.authService.appendAuthorizationHeader(
-      //             req.headers,
-      //             userSecret,
-      //           ),
-      //       }) :
-      //       req),
-      //   switchMap(newReq => next.handle(newReq)),
-      // );
+    if (this.authService.token) {
+      const clonedReq = req.clone({
+        headers: req.headers.set(
+          'Authorization',
+          `Bearer ${this.authService.token}`,
+        ),
+      });
+      return next.handle(clonedReq);
     }
-  }
-
-  /**
-   * Checks if a request is for authorization or refresh token.
-   * @param url - Request url.
-   */
-  private shouldInterceptToken(url: string): boolean {
-    return url.startsWith(this.appConfigService.apiUrl);
+    return next.handle(req);
   }
 }
