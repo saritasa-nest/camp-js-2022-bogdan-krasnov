e9fbb8db6d2c1f6d64b09e5a63167fa963dd4580
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-08-08 10:37:13 +0700

    Update user.mapper.ts Task JC19-425

--- apps/angular/src/core/services/mappers/user.mapper.ts
+++ apps/angular/src/core/services/mappers/user.mapper.ts
@@ -1,22 +1,20 @@
-import { Injectable } from '@angular/core';
-
 import { User } from '../../models/user';
 
 import { UserDto } from './dto/user.dto';
 
-/** User mapper. */
-@Injectable({
-  providedIn: 'root',
-})
-export class UserMapper {
-  /** @inheritdoc */
-  public fromDto(data: UserDto): User {
+export namespace UserMapper {
+
+  /**
+   * Maps dto to model.
+   * @param dto User dto.
+   */
+  export function fromDto(dto: UserDto): User {
     return {
-      id: data.id,
-      firstName: data.first_name,
-      lastName: data.last_name,
-      email: data.email,
-      password: data.password,
+      id: dto.id,
+      firstName: dto.first_name,
+      lastName: dto.last_name,
+      email: dto.email,
+      password: dto.password,
     };
   }
 }
