e44b5a789d76addc4aeb06e16bdd47d059592f9e
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-08-08 14:21:15 +0700

    Update registration.component Task JC19-425

--- apps/angular/src/app/features/auth/registration/registration.component.ts
+++ apps/angular/src/app/features/auth/registration/registration.component.ts
@@ -1,12 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { AuthService } from '../../../../core/services/auth.service';
+
+/** Registration component. */
 @Component({
   selector: 'camp-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css'],
 })
 export class RegistrationComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  /** Register form. */
+  public readonly registerForm = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email, Validators.minLength(6)]),
+    firstName: new FormControl(null, [Validators.required]),
+    lastName: new FormControl(null, [Validators.required]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+    confirmPassword: new FormControl(null, [Validators.required]),
+  });
+
+  constructor(
+    private authService: AuthService,
+  ) {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  public onSubmit(): void {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      this.authService.register({
+        email: String(this.registerForm.value.email),
+        firstName: String(this.registerForm.value.firstName),
+        lastName: String(this.registerForm.value.lastName),
+        password: String(this.registerForm.value.password),
+      }).subscribe();
+    }
+  }
 }
