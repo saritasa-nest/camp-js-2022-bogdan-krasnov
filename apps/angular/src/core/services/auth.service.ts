88a39e3e3e7d413840e707af8240d5e3e9c27fa4
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-08-09 17:10:17 +0700

    Change auth.service Task JC19-425

--- apps/angular/src/core/services/auth.service.ts
+++ apps/angular/src/core/services/auth.service.ts
@@ -1,15 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable, BehaviorSubject } from 'rxjs';
+import { map, Observable, switchMap } from 'rxjs';
 
 import { Login } from '../models/login';
+import { Token } from '../models/token';
 
 import { User } from './../models/user';
 
 import { AppConfigService } from './app-config.service';
-import { UserSecretDto } from './mappers/dto/user-secret.dto';
+import { TokenDto } from './mappers/dto/token.dto';
+import { TokenDataMapper } from './mappers/token-data.mapper';
+import { TokenStorageService } from './token-storage.service';
 
-const TOKEN = 'token';
+const AUTH_LOGIN = 'auth/login/';
+const AUTH_REGISTER = 'auth/register/';
+const AUTH_REFRESH = 'auth/token/refresh/';
+const AUTH_VERIFY = 'auth/token/verify/';
 
 /** Anime service. */
 @Injectable({
@@ -21,57 +27,62 @@ export class AuthService {
 
   private readonly registerUrl: URL;
 
-  private readonly isLoggedIn$ = new BehaviorSubject<boolean>(false);
+  private readonly refreshTokenUrl: URL;
 
-  /** Get token. */
-  public get token(): string | null {
-    return localStorage.getItem(TOKEN);
-  }
+  private readonly accountVerificationUrl: URL;
 
   public constructor(
     appConfig: AppConfigService,
-    private readonly http: HttpClient,
+    private readonly httpClient: HttpClient,
+    private readonly tokenDataMapper: TokenDataMapper,
+    private readonly tokenStorageService: TokenStorageService,
   ) {
-    this.isLoggedIn$.next(!!this.token);
 
-    this.loginUrl = new URL('auth/login/', appConfig.apiUrl);
-    this.registerUrl = new URL('auth/register/', appConfig.apiUrl);
+    this.loginUrl = new URL(AUTH_LOGIN, appConfig.apiUrl);
+    this.registerUrl = new URL(AUTH_REGISTER, appConfig.apiUrl);
+    this.refreshTokenUrl = new URL(AUTH_REFRESH, appConfig.apiUrl);
+    this.accountVerificationUrl = new URL(AUTH_VERIFY, appConfig.apiUrl);
   }
 
   /**
    * Login a user with email and password.
    * @param loginData Login data.
    */
-  public login(loginData: Login): Observable<void> {
-    return this.http.post<UserSecretDto>(
+  public login(loginData: Login): Observable<Token> {
+    return this.httpClient.post<TokenDto>(
       this.loginUrl.toString(),
-      loginData,
+      loginData, // this.loginDataMapper.toDto(loginData),
     ).pipe(
-      map(tokenDto => {
-          localStorage.setItem(TOKEN, tokenDto.access);
-          this.isLoggedIn$.next(true);
-        }),
+      switchMap(token => this.tokenStorageService.saveToken(token)),
+      map(tokenDto => this.tokenDataMapper.fromDto(tokenDto)),
     );
   }
 
   /** Register a user with email and password.
    * @param user User data.
    */
-  public register(user: User): Observable<void> {
-    return this.http.post<UserSecretDto>(
+  public register(user: User): Observable<Token> {
+    return this.httpClient.post<TokenDto>(
       this.registerUrl.toString(),
-      user,
+      user, // this.userDataMapper.toDto(user),
     ).pipe(
-      map(
-        tokenDto => {
-          console.log(tokenDto);
-        },
-      ),
+      switchMap(token => this.tokenStorageService.saveToken(token)),
+      map(dto => this.tokenDataMapper.fromDto(dto)),
     );
   }
 
-  /** Is the user logged in. */
-  public isLoggedIn(): Observable<boolean> {
-    return this.isLoggedIn$.asObservable();
+  /**
+   * Refresh user's token.
+   * @param token Token.
+   * @todo Add refresh token in UserService.
+   * @todo Create RefreshInterceptor.
+   */
+  public refreshToken(token: Token): Observable<Token> {
+    return this.httpClient.post<TokenDto>(
+      this.refreshTokenUrl.toString(),
+      this.tokenDataMapper.toDto(token),
+    ).pipe(
+      map(refreshedToken => this.tokenDataMapper.fromDto(refreshedToken)),
+    );
   }
 }
