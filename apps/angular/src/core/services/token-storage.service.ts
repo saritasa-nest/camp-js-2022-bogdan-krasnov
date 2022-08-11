c81383062ac841b02c9861f052f59e21f9695c6f
bogdan <bogdan.krasnov@saritasa.com>
2022-08-11 10:26:00 +0700

    Create getToken and clearToken Task JC19-425

--- apps/angular/src/core/services/token-storage.service.ts
+++ apps/angular/src/core/services/token-storage.service.ts
@@ -1,12 +1,11 @@
-import { defer, mapTo, Observable, ReplaySubject, tap } from 'rxjs';
+import { concat, defer, mapTo, Observable, race, ReplaySubject, tap, shareReplay } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 import { Token } from '../models/token';
 
 import { LocalStorageService } from './local-storage.service';
 
-// const TOKEN_KEY = 'auth-token';
-const USER_KEY = 'user';
+const TOKEN_STORAGE_KEY = 'auth-token';
 
 /** Token storage service. */
 @Injectable({
@@ -14,24 +13,51 @@ const USER_KEY = 'user';
 })
 export class TokenStorageService {
 
-  /** Current user secret. */
+  private readonly token$: Observable<Token | null>;
+
   private readonly tokenValue$ =
     new ReplaySubject<Token | null>(1);
 
-  public constructor(private readonly storageService: LocalStorageService) { }
+  public constructor(private readonly storageService: LocalStorageService) {
+    this.token$ = this.initTokenStream();
+  }
 
   /** Save token.
    * @param token Token to save.
    */
   public saveToken(token: Token): Observable<Token> {
     return defer(() =>
-      this.storageService.save(USER_KEY, token)).pipe(
+      this.storageService.save(TOKEN_STORAGE_KEY, token)).pipe(
       tap(() => this.tokenValue$.next(token)),
       mapTo(token),
     );
   }
 
-  // public getRefreshToken(): string | null {
-  //   return localStorage.getItem(REFRESHTOKEN_KEY);
-  // }
+  /**
+   * Gets current token.
+   */
+  public getToken(): Observable<Token | null> {
+    return this.token$;
+  }
+
+  /**
+   * Deletes token from storage.
+   */
+     public clearToken(): Observable<void> {
+      return defer(() =>
+        this.storageService.delete(TOKEN_STORAGE_KEY)).pipe(tap(() => this.tokenValue$.next(null)));
+    }
+
+  private initTokenStream(): Observable<Token | null> {
+    const tokenChange$ = this.tokenValue$;
+    const tokenFromStorage$ = concat(
+      defer(() =>
+        this.storageService.get<Token>(TOKEN_STORAGE_KEY)),
+      tokenChange$,
+    );
+
+    return race(tokenChange$, tokenFromStorage$).pipe(
+      shareReplay({ refCount: true, bufferSize: 1 }),
+    );
+  }
 }
