5fea0920d111fdad64db301ea0d084df29f5e9f2
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-08-11 14:07:06 +0700

    Add id in routes Task JC19-435

--- apps/angular/src/app/features/anime/anime.routing.ts
+++ apps/angular/src/app/features/anime/anime.routing.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthorizedGuard } from '../../../core/guards/authorized.guard';
+
 import { AnimeComponent } from './anime.component';
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AnimeComponent,
+  },
+  {
+    path: ':id',
+    component: DetailsComponent,
+    canActivate: [AuthorizedGuard],
   },
 ];
 
