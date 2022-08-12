fbaa68e97ab9a3bf9c01b53c8b66dffc9bbe10ff
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-08-12 12:04:44 +0700

    Create details.component Task JC19-435

--- apps/angular/src/app/features/anime/details/details.component.ts
+++ apps/angular/src/app/features/anime/details/details.component.ts
@@ -1,8 +1,9 @@
-import { Anime } from '@js-camp/core/models/anime';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
 import { map, Observable, switchMap } from 'rxjs';
+
+import { AnimeDetails } from './../../../../core/models/animeDetails';
 
 import { AnimeService } from './../../../../core/services/anime.service';
 
@@ -11,22 +12,25 @@ import { AnimeService } from './../../../../core/services/anime.service';
   selector: 'camp-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DetailsComponent {
 
   /** Anime id. */
   private readonly animeId$: Observable<number> = this.activatedRoute.params.pipe(
-    map((params: Params) => params['id']),
+    map((params: Params) => parseInt(params['id'], 10)),
   );
 
   /** Anime. */
-  public readonly anime$: Observable<Anime> = this.animeId$.pipe(
-    switchMap((userId: number) => this.animeService.getAnimeById(userId)),
-  );
+  public readonly anime$: Observable<AnimeDetails>;
 
   public constructor(
     private readonly activatedRoute: ActivatedRoute,
     private readonly animeService: AnimeService,
   ) {
+    this.anime$ = this.animeId$.pipe(
+      switchMap(id => animeService.getAnimeDetailsById(id)),
+    );
   }
+
 }
