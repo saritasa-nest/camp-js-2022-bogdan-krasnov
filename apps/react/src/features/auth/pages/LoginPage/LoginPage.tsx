ac2306800d0a67f12ee9d05a1e7224f71990487e
bogdan <bogdan.krasnov@saritasa.com>
2022-08-16 16:33:22 +0700

    Create LoginPage Task JC19-456

---
+++ apps/react/src/features/auth/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,10 @@
+import { memo, FC } from 'react';
+
+// Template from MUI docs: https://mui.com/getting-started/templates/
+const LoginPageComponent: FC = () => (
+  <div>
+    <h1>LoginPage</h1>
+  </div>
+);
+
+export const LoginPage = memo(LoginPageComponent);
