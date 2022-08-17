0f07ceaeffd894eaab27bd96c85761a01259ba17
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-08-17 17:26:11 +0700

    Create LocalStorageService Task JC19-456

---
+++ apps/react/src/api/services/localStorageService.ts
@@ -0,0 +1,22 @@
+/** Functionality for working with local storage. */
+export namespace LocalStorageService {
+
+  /**
+   * Gets some value by key from local storage.
+   * @param key The key by which the value is stored.
+   * @returns If there are no value, then null otherwise the value object.
+   */
+  export function getValue<T>(key: string): T | null {
+    const localValue = localStorage.getItem(key);
+    return JSON.parse(localValue ? localValue : 'null');
+  }
+
+  /**
+   * Writes some value by key to local storage.
+   * @param key The key by which the value is stored.
+   * @param value Some value that needs to be stored.
+   */
+  export function setValue<T>(key: string, value: T): void {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+}
