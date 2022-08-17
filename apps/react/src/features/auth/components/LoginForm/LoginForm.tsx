64aabdba6b85963c3036700642db5f021b5e9528
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-08-17 17:31:10 +0700

    Update LoginForm and add dispatch Task JC19-456

--- apps/react/src/features/auth/components/LoginForm/LoginForm.tsx
+++ apps/react/src/features/auth/components/LoginForm/LoginForm.tsx
@@ -1,39 +1,72 @@
-import { memo, FC } from "react";
-import { Formik, Form, Field } from "formik";
-import { initValues, loginFormSchema, LoginFormValue } from "./form-settings";
+import { memo, FC } from 'react';
+import { Formik, Form, Field } from 'formik';
+
+import { Box, Button, Container, Link, Typography } from '@mui/material';
+
+import { TextField } from 'formik-mui';
+
+import { useAppDispatch } from '@js-camp/react/store/store';
+
+import { authLogin } from '@js-camp/react/store/auth/dispatchers';
+
+import { initValues, loginFormSchema, LoginFormValue } from './form-settings';
 
 const LoginFormComponent: FC = () => {
+  const dispatch = useAppDispatch();
+
   const handleUserLogin = (values: LoginFormValue) => {
-    console.log("Form data: ", values);
+    dispatch(authLogin(values));
   };
+
   return (
-    <Formik
-      initialValues={initValues}
-      validationSchema={loginFormSchema}
-      onSubmit={handleUserLogin}
-    >
-      <Form>
-        <Field
-          margin="normal"
-          required
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          autoFocus
-        />
-        <Field
-          margin="normal"
-          required
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-        />
-        <button type="submit">Submit</button>
-      </Form>
-    </Formik>
+    <Container component="main" maxWidth="xs">
+      <Formik
+        initialValues={initValues}
+        validationSchema={loginFormSchema}
+        onSubmit={handleUserLogin}
+      >
+        <Form>
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Field
+              component={TextField}
+              margin="normal"
+              required
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <Field
+              component={TextField}
+              margin="normal"
+              required
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
+              Sign In
+            </Button>
+            <Link href="#" variant="body2">
+              {'No have an account? Sign Up'}
+            </Link>
+          </Box>
+        </Form>
+      </Formik>
+    </Container>
   );
 };
 
