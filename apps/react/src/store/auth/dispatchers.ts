78df8b49e30d14a0297fe007fc1bf2bac5f1c823
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-08-18 16:09:03 +0700

    Add authRegister and update authLogin Task JC19-456

--- apps/react/src/store/auth/dispatchers.ts
+++ apps/react/src/store/auth/dispatchers.ts
@@ -1,9 +1,30 @@
 import { Login } from '@js-camp/core/models/login';
+import { Registration } from '@js-camp/core/models/registration';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
+import { AuthService } from '../../api/services/authService';
 import { UserService } from '../../api/services/userService';
+
+export const fetchUser = createAsyncThunk('auth/fetchUser', () => UserService.getUser());
 
 export const authLogin = createAsyncThunk(
   'auth/login',
-  (loginData: Login) => UserService.loginUser(loginData),
+  (loginData: Login, thunkApi) => {
+    try {
+      return AuthService.login(loginData);
+    } catch (error: unknown) {
+      return thunkApi.rejectWithValue(error);
+    }
+  },
+);
+
+export const authRegister = createAsyncThunk(
+  'auth/register',
+  (registerData: Registration, thunkApi) => {
+    try {
+      return AuthService.register(registerData);
+    } catch (error: unknown) {
+      return thunkApi.rejectWithValue(error);
+    }
+  },
 );
