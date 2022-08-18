28065fe8965afd308279e3922d608f8ba3ec1852
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-08-18 16:06:31 +0700

    Add isLoggedIn and user Task JC19-456

--- apps/react/src/store/auth/state.ts
+++ apps/react/src/store/auth/state.ts
@@ -1,4 +1,4 @@
-import { Token } from '@js-camp/core/models/token';
+import { User } from '@js-camp/core/models/user';
 
 /** Auth state. */
 export interface AuthState {
@@ -6,15 +6,19 @@ export interface AuthState {
   /** Whether authentication is in process or not. */
   readonly isLoading: boolean;
 
+  /** Whether user is logged in or not. */
+  readonly isLoggedIn: boolean;
+
   /** Error message. */
   readonly error: string | null;
 
   /** Whether user is logged in or not. */
-  readonly token: Token | null;
+  readonly user: User | null;
 }
 
 export const initialState: AuthState = {
   isLoading: false,
+  isLoggedIn: false,
   error: null,
-  token: null,
+  user: null,
 };
