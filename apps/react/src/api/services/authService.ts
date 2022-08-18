37e60c80145ca9fd28b5ae5d9770fce53520ae9c
Bogdan <bogdan.krasnov@saritasa@gmail.com>
2022-08-18 16:09:54 +0700

    Update authService Task JC19-456

--- apps/react/src/api/services/authService.ts
+++ apps/react/src/api/services/authService.ts
@@ -4,12 +4,17 @@ import { Token } from '@js-camp/core/models/token';
 import { TokenDto } from '@js-camp/core/dtos/token.dto';
 import { TokenDataMapper } from '@js-camp/core/mappers/token.mapper';
 
+import { Registration } from '@js-camp/core/models/registration';
+
 import { httpClient } from '..';
 
-const AUTH_LOGIN = 'auth/login/';
+import { TokenService } from './tokenService';
 
 /** Auth API. */
 export namespace AuthService {
+
+  const AUTH_LOGIN = 'auth/login/';
+  const AUTH_REGISTER = 'auth/register/';
 
   /**
    * Login a user with email and password.
@@ -17,7 +22,20 @@ export namespace AuthService {
    */
   export async function login(loginData: Login): Promise<Token> {
     const { data } = await httpClient.post<TokenDto>(AUTH_LOGIN, loginData);
-    return TokenDataMapper.fromDto(data);
+    const token = TokenDataMapper.fromDto(data);
+    await TokenService.setToken(token);
+    return token;
+  }
+
+  /**
+   * Register an account.
+   * @param registerData Register data.
+   */
+  export async function register(registerData: Registration): Promise<Token> {
+    const { data } = await httpClient.post<TokenDto>(AUTH_REGISTER.toString(), registerData);
+    const token = TokenDataMapper.fromDto(data);
+    await TokenService.setToken(token);
+    return token;
   }
 
   /** Logs the current user out. */
